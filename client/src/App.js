ab5d945cbd04f3dc85da5f8545150adefe448d51
seanpmcn <46979553+seanpmcn@users.noreply.github.com>
2021-05-26 14:50:15 -0400

    Add css and server

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello
+      <h1>CRUD APPLICATION</h1>
+
+      <div className="form">
+        <label>Movie Name:</label>
+        <input type="text" name="movieName"></input>
+        <label>Review:</label>
+        <input type="text" name="review"></input>
+
+        <button>Submit</button>
+      </div>
     </div>
   );
 }
